87f705e563fbbc715f6e882afd4f24c57b5fa608
Kirrugadu <kiran.06514@gmail.com>
2018-06-28 18:39:22 +0530

    adding file to repo

---
+++ single-module/src/main/java/com/example/Greeter.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+	  
+//what up bitches..  
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
